64f305141d72c434bb41069464d6f026a65484e0
Ryan Viegas <rjkviegas@gmail.com>
2020-07-27 22:22:33 +0100

    tidied up

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -5,8 +5,9 @@ describe Bookmark do
   describe '.all' do
     it 'responds to all method' do
       bookmarks = Bookmark.all
-      expect(bookmarks).to include "https://soundcloud.com/"
+      expect(bookmarks).to include "http://www.makersacademy.com"
       expect(bookmarks).to include "http://www.google.com"
+      expect(bookmarks).to include "http://www.destroyallsoftware.com"
     end
   end
 end
--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -1,7 +1,8 @@
 feature 'viewing bookmarks' do
   scenario 'user navigates to bookmarks page and sees a list' do
     visit '/bookmarks'
-    expect(page).to have_content "https://soundcloud.com/"
+    expect(page).to have_content "http://www.makersacademy.com"
     expect(page).to have_content "http://www.google.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
   end
 end
